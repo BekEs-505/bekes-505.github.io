c24bf40edfaa4329b8fdfc8c92a0cfe99724dcab
System Administrator <BekEs-505>
2024-01-20 19:50:23 +0300

    first commit

--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
-#!/bin/bash
-./push.sh
+#!/bin/sh
+
+dpkg-scanpackages -m ./debs > Packages
+rm -r -f /var/mobile/bekes-505.github.io/Packages.bz2
+bzip2 Packages
+dpkg-scanpackages -m ./debs > Packages
